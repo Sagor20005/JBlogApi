ca7fccabaf280472ca676c7b9ff4dfa8b31415f4
Md Jakareya <109013040+Sagor20005@users.noreply.github.com>
2024-08-15 16:48:02 +0600

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors")
 const router = require('./routes')
 
-let PORT = 2001555;
+let PORT = 6555;
 const app = express()
 app.use(express.json())
 app.use(express.static("upload"))
